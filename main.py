60e84930e7199a1b4c2a248a9ed1c1dda4634ec4
arthurarty <arthur.nangai@gmail.com>
2020-11-19 12:44:21 +0300

    Add accept_only_tky_requests.

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from flask import Flask, request, jsonify
+import functools
 
 
 app = Flask(__name__)
 
 
+def accept_only_tyk_requests(func):
+    """
+    Check header for Tyk-Backend header
+    :param func:
+    :return:
+    """
+
+    @functools.wraps(func)
+    def check_header(*args, **kwargs):
+        tyk_backend_key = request.headers.get('Tyk-Backend-Key')
+        if not tyk_backend_key == 'secret-key':
+            return jsonify('Forbidden'), 403
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return check_header
+
+
 @app.route('/')
+@accept_only_tyk_requests
 def hello_world():
-    tyk_backend_key = request.headers.get('Tyk-Backend-Key')
-    if not tyk_backend_key == 'secret-key':
-        return jsonify('Forbidden'), 403
     return jsonify('Hello world!'), 200
 
 
