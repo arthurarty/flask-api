3fdb8226eff1525772e1b814f03b165813aa6440
arthurarty <arthur.nangai@gmail.com>
2020-11-19 11:16:20 +0300

    check header

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World'
+    tyk_backend_key = request.headers.get('Tyk-Backend-Key')
+    if not tyk_backend_key == 'secret-key':
+        return jsonify('Forbidden'), 403
+    return jsonify('Hello world!'), 200
 
 
 if __name__ == '__main__':
