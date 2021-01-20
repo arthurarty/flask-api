c3d636e477c93efc0b7dac5aebbc300d452205e6
arthurarty <arthur.nangai@gmail.com>
2021-01-20 11:26:57 +0300

    Accept both get and post.

--- main.py
+++ main.py
@@ -34,15 +34,15 @@ def hello_world():
     return jsonify('Hello world!'), 200
 
 
-@app.route('/login/', methods=['POST'])
+@app.route('/login/', methods=['POST', 'GET'])
 @swag_from('docs/login.yml')
 def tyk_login_with_email():
     """
     Login method servers a firebase proxy so that users can access the tyk
     dashboard using already existing console(firebase) credentials.
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.args.get('email') or request.form.get('email')
+    password = request.args.get('password') or request.form.get('password')
     if not email or not password:
         return (
             jsonify(
